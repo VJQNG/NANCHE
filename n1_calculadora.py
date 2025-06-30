7775e68a843c93821deac5064cee67b16ed106c0
Marcos Aguilar <marcosyaeldelaluzaguilar@gmail.com>
2025-06-30 00:41:49 -0600

    Avances: funcion: extraer_caracteres()

--- n1_calculadora.py
+++ n1_calculadora.py
@@ -1,1 +1,24 @@
-Subir archivo
+'''
+Esta será una calculadora, ademas de prácticas python otra vez
+'''
+def extraer_caracteres(a):
+    caracteres = [x for x in a]
+    espera = ''
+    op = 0
+    for relleno in caracteres:
+        if relleno in '0123456789':
+            espera += relleno
+            caracteres.pop(0)
+        else:
+            break
+    for cosa in caracteres:
+        if cosa == '+':
+            pass
+        
+
+
+
+
+print("Calculadora\n")
+entrada = input(str('ingrese: '))
+extraer_caracteres(entrada)
