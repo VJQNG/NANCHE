06b6a0926d87b019e1ba28cbd379344ce65b6241
Marcos Aguilar <marcosyaeldelaluzaguilar@gmail.com>
2025-07-03 22:27:21 -0600

    cambios en extraer_caracteres para obtener el primer operando de la lista de valores para hacer la operacion, me quedé en el segundo for para hacer las operaciones

--- n1_calculadora.py
+++ n1_calculadora.py
@@ -2,24 +2,21 @@
 Esta será una calculadora, ademas de practicar python otra vez.
 '''
 def extraer_caracteres(a):
-    c = [x for x in a]
-    espera = ''
-    op = 0
-    print(c)
-    for relleno in c:
-        if relleno in '+-*/':
-            print(f'caracteres: {c}, espera: {espera}')
+    c = [x for x in a] # lista de valores ingresados por el usuario.
+    espera = '' # primer operando para hacer el siguiente.
+    op = 0 # guarda la operacion
+    for pos in c:
+        if(pos in '+-*/'):
+            print(f'caracteres break: {c}, espera: {espera}') 
+            del c[0:len(espera)+1]
+            print(c)
+            for hacer in range(len(c)):
+                match hacer:
+                    case '+':
+                        pass
             break
         else:
-            print(f'caracteres: {c}, espera: {espera}')
-            espera += relleno
-            c.pop(0)
-            print(f'caracteres: {c}, espera: {espera}')
-
-
-
-
-
+            espera += pos
 
 print("Calculadora\n")
 entrada = input(str('ingrese: '))
